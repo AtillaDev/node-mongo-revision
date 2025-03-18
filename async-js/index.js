89f78aedcf081373523e9d6c3ef5c1097d335586
Atila Machan <atiladev@abv.bg>
2025-03-18 23:37:32 +0200

    promises constructor

--- async-js/index.js
+++ async-js/index.js
@@ -1,6 +1,45 @@
 const fs = require('fs');
 const superagent = require('superagent');
 
+const readFilePro = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) {
+        reject('I could not found that file');
+      }
+      resolve(data);
+    });
+  });
+};
+
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) {
+        reject('Could not write file');
+      }
+      resolve('success');
+    });
+  });
+};
+
+readFilePro(`${__dirname}/dog.txt`)
+  .then((data) => {
+    console.log(`Breed: ${data}`);
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  })
+  .then((res) => {
+    console.log(res.body.message);
+    return writeFilePro('./async-js/dog-img.txt', res.body.message);
+  })
+  .then(() => {
+    console.log('Random dog image');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// Callback hell
 // fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
 //   console.log(`Breed: ${data}`);
 
@@ -15,19 +54,19 @@ const superagent = require('superagent');
 //     });
 // });
 
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
-  console.log(`Breed: ${data}`);
+// fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+//   console.log(`Breed: ${data}`);
 
-  superagent
-    .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .then((res) => {
-      console.log(res.body.message);
+//   superagent
+//     .get(`https://dog.ceo/api/breed/${data}/images/random`)
+//     .then((res) => {
+//       console.log(res.body.message);
 
-      fs.writeFile('dog-img.txt', res.body.message, (err) => {
-        console.log('Random dog image');
-      });
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
-});
+//       fs.writeFile('dog-img.txt', res.body.message, (err) => {
+//         console.log('Random dog image');
+//       });
+//     })
+//     .catch((err) => {
+//       console.log(err.message);
+//     });
+// });
